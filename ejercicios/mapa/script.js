31e9726f2bbf47b6e33d59881e5be4e95c2cb2a3
underscoreFaie <underscorefaie@gmail.com>
2025-12-01 15:21:14 +0100

    Añadida variante de ejercicio Mapa y correcciones de estilo

--- ejercicios/mapa/script.js
+++ ejercicios/mapa/script.js
@@ -1,6 +1,5 @@
 import ubicaciones from "./almacen.js";
 
-let marcadores;
 const marcador_img= 'img/location-pin.png';
 
 const test= document.querySelector('[data-class="test"]');
@@ -8,28 +7,38 @@ const mapContainer= document.querySelector('[data-class="mapContainer"]');
 const map= document.querySelector('[id="map"]');
 const menuButton= document.querySelector('[data-class="menuButton"]');
 const mapMenu= document.querySelector('[id="map-menu"]');
+const confirm= document.getElementById('confirm');
+const nombre_input= document.getElementById('name-input');
+const description_input= document.getElementById('description-input');
+const message= document.getElementById('message');
+
+let num_marcadores=0;
 
 document.addEventListener('DOMContentLoaded', mostrarMarcadores);
-menuButton.addEventListener('click', mostrarMenu)
-
+menuButton.addEventListener('click', mostrarMenu);
+confirm.addEventListener('click', evt => {
+    message.style.display= "block";
+    map.addEventListener('click', anadirMarcador);
+});
 
 function mostrarMarcadores() {
 /* Para cada entrada de marcadores añade un marcador a mapContainer, lo posiciona y añade los listeners para mostrar y eliminar la información adicional
 */
     for (let i=0; i<ubicaciones.length; i++) {
-        let marcador= ubicaciones[i]
+        let marcador= ubicaciones[i];
 
         //Crear div
-        let marcador_html= generarDiv(marcador, i);
+        let marcador_html= generarDiv(marcador, num_marcadores);
         
         //Añadir div al documento
         mapContainer.appendChild(marcador_html);
         marcador_html.addEventListener('mouseenter', ()=> mostrarContenido(i));
         marcador_html.addEventListener('mouseleave', eliminarContenido);
+        num_marcadores++;
     }
 }
 
-function generarDiv(marcador, i) {
+function generarDiv(marcador, i, is_new=false) {
     //TODO - Hacer un switch sobre marcador.type para cambiar el tipo de marcador
     let marcador_html= document.createElement('div');
         marcador_html.id= 'marker' + (i+1);
@@ -38,8 +47,14 @@ function generarDiv(marcador, i) {
             <img class="marker_img" src="${marcador_img}">`;
         
         //Calcular posición (como porcentaje)
-        let top= 100*marcador.coordY/map.naturalHeight;
-        let left= 100*marcador.coordX/map.naturalWidth;
+        let top= marcador.coordY, left= marcador.coordX;
+        if (!(is_new)) {
+            top= 100*top/map.naturalHeight;
+            left= 100*left/map.naturalWidth;
+        } else {
+            top= 100*top/map.clientHeight;
+            left= 100*left/map.clientWidth;
+        }
         //Poner posición
         marcador_html.style.position= 'absolute';
         marcador_html.style.top= top + '%';
@@ -47,8 +62,9 @@ function generarDiv(marcador, i) {
     return marcador_html;
 }
 
-function mostrarContenido(i) {
+function mostrarContenido(i, is_new=false) {
 // Añade un div con la información adicional a mapContainer
+    test.innerText= i;
     let marcador= ubicaciones[i];
 
     //Crear div
@@ -58,8 +74,15 @@ function mostrarContenido(i) {
         <h3 id='name'>${marcador.name}</h3>
         <div id='description'>${marcador.description}</div>`
     //Calcular posición
-    let top= 100*marcador.coordY/map.naturalHeight;
-    let left= 100*marcador.coordX/map.naturalWidth;
+    let top= marcador.coordY, left= marcador.coordX;
+    if (!(is_new)) {
+        top= 100*top/map.naturalHeight;
+        left= 100*left/map.naturalWidth;
+    } else {
+        top= 100*top/map.clientHeight;
+        left= 100*left/map.clientWidth;
+    }
+    
     //Poner posición
     info_html.style.position= 'absolute';
     info_html.style.top= top + '%';
@@ -82,9 +105,26 @@ function mostrarMenu() {
     else mapMenu.style.left= '-500%';
 }
 
-function anadirMarcador() {
-    /* Añade un nuevo marcador
-    * TODO - Añadir un formulario al menú. Leer resultados y añadir el marcador
+function anadirMarcador(evt) {
+    /* Lee el contenido del formulario y añade un nuevo marcador en la posición del ratón
     */
-   
+    let posX= evt.offsetX;
+    let posY= evt.offsetY;
+    test.innerText= posX + '(' + map.naturalHeight + ')' + ', ' + posY;
+    let marcador= {
+        "name": nombre_input.value,
+        "description": description_input.value,
+        "coordX": posX,
+        "coordY": posY,
+        "type": 1};
+    ubicaciones.push(marcador);
+    let marcador_html= generarDiv(marcador, num_marcadores, true);
+    mapContainer.appendChild(marcador_html);
+    const i= num_marcadores;
+    marcador_html.addEventListener('mouseenter', ()=> mostrarContenido(i, true));
+    marcador_html.addEventListener('mouseleave', eliminarContenido);
+    num_marcadores++;    
+    
+    message.style.display= "none";
+    map.removeEventListener('click', anadirMarcador);
 }
